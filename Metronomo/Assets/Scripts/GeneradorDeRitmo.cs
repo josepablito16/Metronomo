debc4a0f8fc42da038b6273aa566e9d5df66d215
josepablito16 <josepablocif16@gmail.com>
2021-08-12 16:44:55 -0600

    Codigo de metronomo adaptado a la nueva escena

--- Metronomo/Assets/Scripts/GeneradorDeRitmo.cs
+++ Metronomo/Assets/Scripts/GeneradorDeRitmo.cs
@@ -5,12 +5,71 @@ using System.Linq;
 
 
 public class GeneradorDeRitmo : MonoBehaviour
-{
+{   
+    [Header("Samples")]
+    public AudioSource[] MetroSound;
+
+    private double Interval; //The interval between the ticks
+
+    private double SubInterval;
+
+    private double BPM = 120;
+
+    private int Counter;
+
+    private int setTheTime = 1000;
+
+    private int NumberOfBeatsInBar = 2; //cant_subdivision
+
+    private int contadorControl = 0;
+
+    private IEnumerator IEtickRoutine;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        generarRitmo();        
-        
+        Interval = 60.0f / BPM;
+        SubInterval = Interval/2;
+        IEtickRoutine = tickRoutine();
+        StartCoroutine(IEtickRoutine);
+
+        generarRitmo();
+
+    }
+
+    IEnumerator tickRoutine()
+    {
+        //While the time is less than 1000( You can use any number for this, but it seems to multiply it by 2 what ever you pick) this is the amount of time the metronome runs for
+        while (Time.time < setTheTime) //decided to use a while statement because it seemed like the bst option for a continuingly playing beat
+        {
+
+            // Subintervalo
+            MetroSound[2].volume = 1;
+            MetroSound[2].Play();
+
+
+
+            //Intervalo normal
+            contadorControl++;
+            if (contadorControl % 2 == 0)
+            {
+                Counter++;
+                if (Counter % NumberOfBeatsInBar == 1)             //on the first beat I want to play a different sound, then repeat that pattern every 4 beats 1(Accent), 2, 3, 4, 1(Accent), 2, 3, 4.... etc.  In this case I have used a modulas operator. 
+                {
+                    MetroSound[0].Play();
+                }
+                else
+                {
+                    MetroSound[1].Play();
+                }
+
+            }
+
+            Interval = 60.0f / BPM;
+            SubInterval = Interval/2;
+            yield return new WaitForSecondsRealtime((float)SubInterval); // Because I decided to use doubles for the ticks and BPM to be more acurate with time, we have to explicitly imply it as a float for the WaitForSecondsRealtime method to recognose it.
+        }
     }
 
     // Update is called once per frame
@@ -121,6 +180,24 @@ public class GeneradorDeRitmo : MonoBehaviour
         return relleno;
     }
 
+    int[] getClaveArray(int[] relleno)
+    {
+        int[] claveArray = new int[relleno.Length];
+
+        int contador = 0;
+        foreach (int i in relleno)
+        {
+            if(i==0)
+                claveArray[contador] = 1;
+            else
+                claveArray[contador] = 0;
+            
+            contador += 1;
+        }
+
+        return claveArray;
+    }
+
     void crearClave(int cantidadSubdivision, int subdivisionBase)
     {
         int[] posibleSubdivision = {1,2,4};
@@ -140,6 +217,7 @@ public class GeneradorDeRitmo : MonoBehaviour
         int[] relleno = crearRelleno(claveFinal,subdivisionRandom * cantidadSubdivision);
         Debug.Log("Relleno final = "+string.Join(", ", relleno));
 
-
+        int[] claveArray = getClaveArray(relleno);
+        Debug.Log("Clave array = "+string.Join(", ", claveArray));
     }
 }
